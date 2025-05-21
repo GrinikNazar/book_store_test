9d4f2da53a3639fe59c9c3c87e8fb2f510b4c4db
GrinikNazar <hrynyknazar@gmail.com>
2025-05-21 21:56:54 +0300

    create new models and learnt about relations

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Author, Address, Country
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,4 +10,19 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'rating')
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'address')
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('street', 'phone_number', 'city')
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', )
+
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country, CountryAdmin)
